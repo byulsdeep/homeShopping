34e4bd0694435b43cd0360c3383322bba95f4e21
Soon <73885789+lag2331@users.noreply.github.com>
2023-04-03 12:00:01 +0900

    Revert "bean 생성"

--- src/com/gelato/action/Action.java
+++
@@ -1,8 +0,0 @@
-package com.gelato.action;
-
-import javax.servlet.http.*;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response)
-			throws Exception;
-}
